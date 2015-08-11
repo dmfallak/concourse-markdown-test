1db4b307f027d7bdea58b6dfda1cc5d8424af232
Devin Fallak <pair+dfallak@pivotal.io>
2015-08-11 15:39:50 -0400

    Adds sleeps

--- show-me-kittens.sh
+++ show-me-kittens.sh
@@ -1,10 +1,19 @@
 echo "Initializing kittens..."
+sleep 1
 echo "Kittens initialized!"
+sleep 1
 echo "Retrieving kittens..."
+sleep 1
 echo "Kittens retrieved!"
+sleep 1
 echo "Stand by for kittens!"
+sleep 1
 echo "![Here's a kitten!](http://placekitten.com/g/200/300)"
+sleep 1
 echo "![More coming](http://placekitten.com/g/150/350)"
+sleep 1
 echo "![One more after this](http://placekitten.com/g/350/300)"
+sleep 1
 echo "![Big one](http://placekitten.com/g/500/500)"
+sleep 1
 echo "Hope you enjoyed those kittens!"
