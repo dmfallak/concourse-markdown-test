2aa7a3d9b9ac40bab6504b16a8dde97695321fd2
Devin Fallak <pair+dfallak@pivotal.io>
2015-08-11 16:46:52 -0400

    Use sh not bash

--- on-failure.sh
+++ on-failure.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "![Deployment failed](http://i.giphy.com/A1SxC5HRrD3MY.gif)"
--- on-success.sh
+++ on-success.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "![Successful deployment](http://i.giphy.com/zaqclXyLz3Uoo.gif)"
